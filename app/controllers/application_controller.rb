e5544fff3399590418095bad14130131c5a69754
svbergstrom <sbergstr@chicagobooth.edu>
2021-04-21 04:29:44 +0000

    progress on paper page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+    
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end
+
+
   def play_rock
     
     #render({ :html => "<h1>Hello, world!</h1>".html_safe })
@@ -11,5 +17,25 @@ class ApplicationController < ActionController::Base
 
     render({ :template => "game_templates/user_rock.html.erb"})
   end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render({ :template => "game_templates/user_paper.html.erb"})
+  end
+
+  def play_scissors
+  render({ :template => "game_templates/user_scissors.html.erb"})
+  end
+
+
 
 end
