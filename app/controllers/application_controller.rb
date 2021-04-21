71da684551d34c0c5ccfc38faead206b383fd9fc
svbergstrom <sbergstr@chicagobooth.edu>
2021-04-21 03:56:38 +0000

    rps rcav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    
+    #render({ :html => "<h1>Hello, world!</h1>".html_safe })
+    #redirect_to("https://www.wikipedia.org")
+
+    render({ :template => "game_templates/user_rock.html.erb"})
+  end
+
 end
